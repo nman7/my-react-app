fb9d592846cb69e3db5a3ded4927d40b786a7469
nauman mansuri <naumanmansuri@nauman-ka-MacBook-Pro.local>
2024-12-16 14:00:13 +1100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div style={{ textAlign: 'center', padding: '20px' }}>
+            <h1>Welcome to My Dummy React App</h1>
+            <p>This is a simple React project containerized with Docker and deployed for free.</p>
+        </div>
+    );
+}
+
+export default App;
+
